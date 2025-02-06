257b4a7701a34f353c82bc0062266b4713f1596a
Kristjan Greenewald <Kristjan.H.Greenewald@ibm.com>
2025-02-06 16:33:13 -0500

    Update finetune_alora.py

--- alora_intrinsics/finetune_alora.py
+++ alora_intrinsics/finetune_alora.py
@@ -5,17 +5,18 @@ from datasets import Dataset, DatasetDict, load_from_disk, concatenate_datasets
 
 from sklearn.model_selection import train_test_split
 from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
-from alora_intrinsics.alora.peft_model_alora import PeftModelForCausalLM
+from alora_intrinsics.alora.peft_model_alora import PeftModelForCausalLM as aLoRAPeftModelForCausalLM
 from alora_intrinsics.alora.config import aLoraConfig
+# standard lora
+from peft import PeftModelForCausalLM, LoraConfig
 import json
 
 from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
 
-
+adapter = 'aLoRA' #'LoRA'
 
 DATA_PATH = os.getenv("HF_DATASETS_CACHE")
-MODEL_NAME = "/proj/dmfexp/statllm/users/kgreenewald/models/granite-3.1-8b-instruct-r241212a"#"ibm-granite/granite-3.0-8b-instruct"
-
+MODEL_NAME = "ibm-granite/granite-3.1-8b-instruct"
 
 CERTAINTY_PROMPT = "<|start_of_role|>certainty<|end_of_role|>"
 SAFETY_PROMPT = "<|start_of_role|>safety<|end_of_role|>"
@@ -59,13 +60,14 @@ def process_datasets(datasets,tokenizer,max_rows):
             convo = ds["conversations"][i]
             if convo[0]["role"] != "system":
                 convo = [{"role":"system", "content":""}] + convo# "You are an AI language model developed by IBM Research. You are a cautious assistant. You carefully follow instructions. You are helpful and harmless and you follow ethical guidelines and promote positive behavior."}] + convo
-                string = tokenizer.apply_chat_template(convo[:-1], tokenize=False,add_generation_prompt=False)
-                string_to_remove = tokenizer.apply_chat_template(convo[0:1], tokenize=False,add_generation_prompt=False)
-                string = string[len(string_to_remove):]
-                print('hi')
-                print(string)
-            else:
-                string = tokenizer.apply_chat_template(convo[:-1], tokenize=False,add_generation_prompt=False)
+                #string = tokenizer.apply_chat_template(convo[:-1], tokenize=False,add_generation_prompt=False)
+                
+                #print('hi')
+                #print(string)
+            #else:
+            string = tokenizer.apply_chat_template(convo[:-1], tokenize=False,add_generation_prompt=False)
+            string_to_remove = tokenizer.apply_chat_template(convo[0:1], tokenize=False,add_generation_prompt=False)
+            string = string[len(string_to_remove):]
             if convo[-1]["role"] == "Hallucination_tag":
                 convo[-1]["role"] = "hallucination"
                 if convo[-1]["content"] == "1":
@@ -77,9 +79,9 @@ def process_datasets(datasets,tokenizer,max_rows):
             if convo[-1]["role"] == "SAFETY_EXCEPTION":
                 convo[-1]["role"]= "safety"
                 if convo[-1]["content"] == "1":
-                    convo[-1]["content"] = "No, this is unsafe!"
+                    convo[-1]["content"] = "N"#"No, this is unsafe!"
                 else:
-                    convo[-1]["content"] = "Yes, this is a safe prompt."
+                    convo[-1]["content"] = "Y"#es, this is a safe prompt."
             inputs.append(string + "<|start_of_role|>" + convo[-1]["role"] + "<|end_of_role|>" )
             targets.append(convo[-1]["content"]+'<|end_of_text|>')
         proc_dict = dict()
@@ -113,19 +115,19 @@ def SFT_data(int_name):
 
 
 
-    if 1: #LORA:
+    
        
    
-        model_name = MODEL_NAME
+    model_name = MODEL_NAME
 
-        token = os.getenv("HF_MISTRAL_TOKEN")
-        model_dir = model_name #os.path.join(DMF_MODEL_CACHE, model_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_dir,padding_side='left',trust_remote_code=True,token=token)
+    token = os.getenv("HF_MISTRAL_TOKEN")
+    model_dir = model_name #os.path.join(DMF_MODEL_CACHE, model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_dir,padding_side='left',trust_remote_code=True,token=token)
         
-        model_base = AutoModelForCausalLM.from_pretrained(model_dir,device_map = 'auto', use_cache=False)
-        tokenizer.pad_token = tokenizer.eos_token
-        model_base.config.pad_token_id = model_base.config.eos_token_id
-        tokenizer.add_special_tokens = False
+    model_base = AutoModelForCausalLM.from_pretrained(model_dir,device_map = 'auto', use_cache=False)
+    tokenizer.pad_token = tokenizer.eos_token
+    model_base.config.pad_token_id = model_base.config.eos_token_id
+    tokenizer.add_special_tokens = False
     datasets = process_datasets(data,tokenizer,max_rows = 400000)
     if int_name == "certainty":
         merged_dataset = datasets[0]
@@ -143,33 +145,80 @@ def SFT_data(int_name):
     
     collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
    
-  
+    if adapter == 'aLoRA': # aLoRA model
+        peft_config = aLoraConfig(
+            r=32,
+            lora_alpha=32,
+            lora_dropout=0.05,
+            bias="none",
+            task_type="CAUSAL_LM",
+            target_modules=["q_proj","k_proj", "v_proj"],#Can only do q, k, v layers (for now).
+            #layers_to_transform=[38,39]
+        )
+        response_tokens = tokenizer(response_template, return_tensors="pt", add_special_tokens=False)
+        response_token_ids = response_tokens['input_ids']
+        peft_model = aLoRAPeftModelForCausalLM(model_base, peft_config,response_token_ids = response_token_ids)
+        trainer = SFTTrainer(
+            peft_model,
+            train_dataset=merged_dataset,
+            args=SFTConfig(output_dir="/proj/dmfexp/statllm/users/kgreenewald/Thermometer/tmp",dataset_kwargs={"add_special_tokens":False},num_train_epochs=3,learning_rate=6e-7*2,max_seq_length = 4096,per_device_train_batch_size = 3,save_strategy="no",gradient_accumulation_steps=8,fp16=True),
+            formatting_func=formatting_prompts_func,
+        data_collator=collator
+        #,
+        )
+        trainer.train()
+    
+        peft_model.save_pretrained(SAVE_PATH + "/feb6_8bsft_alora_sz32"+ int_name)
+    else: #standard LoRA. THESE HYPERPARAMETERS ARE NOT TUNED
+        peft_config = LoraConfig(
+            r=6,
+            lora_alpha=32,
+            lora_dropout=0.05,
+            bias="none",
+            task_type="CAUSAL_LM",
+            target_modules=["q_proj","k_proj", "v_proj"],
+            #layers_to_transform=[38,39]
+        )
+        peft_model = PeftModelForCausalLM(model_base, peft_config)
+        trainer = SFTTrainer(
+            peft_model,
+            train_dataset=merged_dataset,
+            args=SFTConfig(output_dir="/proj/dmfexp/statllm/users/kgreenewald/Thermometer/tmp",dataset_kwargs={"add_special_tokens":False},num_train_epochs=1,learning_rate=6e-5,max_seq_length = 4096,per_device_train_batch_size = 1,save_strategy="no",gradient_accumulation_steps=8,fp16=True),
+            formatting_func=formatting_prompts_func,
+        data_collator=collator
+        #,
+        )
+        trainer.train()
+    
+        peft_model.save_pretrained(SAVE_PATH + "/feb6_8bsft_standard_lora_sz6"+ int_name)
+        
 
-    peft_config = aLoraConfig(
-        r=32,
-        lora_alpha=32,
-        lora_dropout=0.05,
-        bias="none",
-        task_type="CAUSAL_LM",
-        target_modules=["q_proj","k_proj", "v_proj"],#Can only do q, k, v layers (for now).
-        #layers_to_transform=[38,39]
-    )
-    response_tokens = tokenizer(response_template, return_tensors="pt", add_special_tokens=False)
-    response_token_ids = response_tokens['input_ids']
-    peft_model = PeftModelForCausalLM(model_base, peft_config,response_token_ids = response_token_ids)
-    trainer = SFTTrainer(
-        peft_model,
-        train_dataset=merged_dataset,
-        args=SFTConfig(output_dir="/proj/dmfexp/statllm/users/kgreenewald/Thermometer/tmp",dataset_kwargs={"add_special_tokens":False},num_train_epochs=6,learning_rate=6e-7,max_seq_length = 4096,per_device_train_batch_size = 1,save_strategy="no",gradient_accumulation_steps=8,fp16=True),
-        formatting_func=formatting_prompts_func,
-    data_collator=collator
-    #,
-    )
+
+    # peft_config = aLoraConfig(
+    #     r=32,
+    #     lora_alpha=32,
+    #     lora_dropout=0.05,
+    #     bias="none",
+    #     task_type="CAUSAL_LM",
+    #     target_modules=["q_proj","k_proj", "v_proj"],#Can only do q, k, v layers (for now).
+    #     #layers_to_transform=[38,39]
+    # )
+    # response_tokens = tokenizer(response_template, return_tensors="pt", add_special_tokens=False)
+    # response_token_ids = response_tokens['input_ids']
+    # peft_model = PeftModelForCausalLM(model_base, peft_config,response_token_ids = response_token_ids)
+    # trainer = SFTTrainer(
+    #     peft_model,
+    #     train_dataset=merged_dataset,
+    #     args=SFTConfig(output_dir="/proj/dmfexp/statllm/users/kgreenewald/Thermometer/tmp",dataset_kwargs={"add_special_tokens":False},num_train_epochs=6,learning_rate=6e-7,max_seq_length = 4096,per_device_train_batch_size = 1,save_strategy="no",gradient_accumulation_steps=8,fp16=True),
+    #     formatting_func=formatting_prompts_func,
+    # data_collator=collator
+    # #,
+    # )
 
  
-    trainer.train()
+    # trainer.train()
     
-    peft_model.save_pretrained("/proj/dmfexp/statllm/users/kgreenewald/Thermometer/models/alora/8bsft_aloraV2_sz32"+ int_name)
+    # peft_model.save_pretrained("/proj/dmfexp/statllm/users/kgreenewald/Thermometer/models/alora/8bsft_aloraV2_sz32"+ int_name)
     
 
 
