5a8b7dc994eac26e43d9bb2977d706e1cdae8f99
Kristjan Greenewald <Kristjan.H.Greenewald@ibm.com>
2025-03-26 13:13:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='activated_lora',
+    version='0.1.0',
+    description='A short description of activated_lora',
+    author='Kristjan Greenewald',
+    author_email='kristjan.h.greenewald@ibm.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add any required dependencies here
+    ],
+    python_requires='>=3.8',
+)
